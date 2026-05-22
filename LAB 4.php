1f8765397511ebc67e74d6cfcefc38a757b48094
Kiel-git <Johngmmabalaba@gmail.com>
2026-05-22 21:11:20 +0800

    Add PHP script to check if a number is even or odd

---
+++ LAB 4.php
@@ -0,0 +1,11 @@
+<?php
+// Input a number
+$number = 7; // You can change this value or get user input
+
+// Check if the number is even or odd using modulus operator
+if ($number % 2 == 0) {
+    echo "$number is even";
+} else {
+    echo "$number is odd";
+}
+?>
