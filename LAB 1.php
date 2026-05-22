c243a9c089cd0f9fcf91df3910fd1440fde988f6
Kiel-git <Johngmmabalaba@gmail.com>
2026-05-22 21:07:12 +0800

    Create LAB 1.php

---
+++ LAB 1.php
@@ -0,0 +1,10 @@
+<?php
+// Create a variable $age with value 18
+$age = 18;
+
+// Check if age is 18 or above
+if ($age >= 18) {
+    // Display the message if condition is true
+    echo "You are eligible to vote";
+}
+?>
